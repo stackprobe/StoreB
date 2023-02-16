72aa2fd1ad63433c6f8cb47f85e0f5333700fce0
stackprobe <stackprobes@gmail.com>
2023-02-17 01:58:03 +0900

    Voyager 1 is 23799509458.922 kilometers away from the Sun.

---
+++ Dev/GameJS/SlotMachine/Gattonero20200001/_index_Release.html.js
@@ -0,0 +1,87 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />
+<script>
+
+function Gattonero20200001_Run()
+{
+
+'use strict'
+
+@(SCRIPT)
+
+}
+
+window.onload = function()
+{
+	Gattonero20200001_Run();
+};
+
+</script>
+</head>
+<body style="
+	overscroll-behavior-y: none;
+	touch-action: manipulation;
+	">
+<center>
+<div id="Gattonero20200001-CanvasBox"></div>
+</center>
+
+<hr/>
+
+<br/>
+
+<strong>スロットマシン・テスト実装</strong><br/>
+
+<br/>
+
+<div style="color: #a00000;">
+レート<br/>
+====<br/>
+7 BONUS 青 : x 10000<br/>
+7 BONUS 赤 : x 3000<br/>
+BAR : x 1000<br/>
+スイカ 青 : x 300<br/>
+スイカ 赤 : x 100<br/>
+ベル 青 : x 30<br/>
+ベル 黄 : x 10<br/>
+さくらんぼ 緑 : x 3<br/>
+さくらんぼ 赤 : x 2<br/>
+</div>
+
+<br/>
+
+タイトル・メニューの背景：<br/>
+　フリー素材ぱくたそ<br/>
+　https://www.pakutaso.com/<br/>
+
+<br/>
+
+スロットの絵柄：<br/>
+　春一番のフリー素材工房<br/>
+　https://commons.nicovideo.jp/material/nc226455<br/>
+
+<br/>
+
+エフェクト：<br/>
+　ぴぽや<br/>
+　https://pipoya.net/<br/>
+
+<br/>
+
+音楽：<br/>
+　フリーBGM・音楽素材MusMus<br/>
+　https://musmus.main.jp<br/>
+
+<br/>
+
+効果音：<br/>
+　小森平<br/>
+　https://taira-komori.jpn.org/<br/>
+
+<br/>
+<br/>
+<br/>
+
+</body>
+</html>
---
+++ Dev/Program/Test20230111/Claes20200001/Claes20200001/Tests/Test0005.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Charlotte.Commons;
+
+namespace Charlotte.Tests
+{
+	public class Test0005
+	{
+		public void Test01()
+		{
+			Console.WriteLine(string.Join(" ", SCommon.Serializer.I.Split("7EAGNjYGDwSM3JyddhBbESCwoq2YCM8PyinBRFAJsSOoAd4")));
+		}
+	}
+}
