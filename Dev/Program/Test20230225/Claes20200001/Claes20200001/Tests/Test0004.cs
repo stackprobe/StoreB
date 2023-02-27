f4269f494beec5387b8ebe62ede1204143c2359a
stackprobe <stackprobes@gmail.com>
2023-02-27 22:58:02 +0900

    Voyager 1 is 23815380877.133 kilometers away from the Sun.

--- Dev/Program/Test20230225/Claes20200001/Claes20200001/Tests/Test0004.cs
+++ Dev/Program/Test20230225/Claes20200001/Claes20200001/Tests/Test0004.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Charlotte.Utilities;
-using Charlotte.Commons;
 using System.IO;
+using Charlotte.Commons;
+using Charlotte.Utilities;
 
 namespace Charlotte.Tests
 {
@@ -14,43 +14,73 @@ namespace Charlotte.Tests
 
 		public void Test01()
 		{
-			string[][] rows1;
-			string[][] rows2;
+			string file1 = @"C:\temp\1.csv";
+			string file2 = @"C:\temp\2.csv";
 
-			using (CsvFileReader reader = new CsvFileReader(@"C:\temp\Customers.csv"))
 			{
-				rows1 = reader.ReadToEnd();
+				string[][] rows;
+
+				using (CsvFileReader reader = new CsvFileReader(@"C:\temp\Customers.csv"))
+				{
+					rows = reader.ReadToEnd();
+				}
+
+				Array.Sort(rows, (a, b) => SCommon.Comp(a[0], b[0]));
+				using (CsvFileWriter writer = new CsvFileWriter(file1))
+				{
+					writer.WriteRows(rows);
+				}
 			}
 
-			File.WriteAllText(@"C:\temp\1.txt", "SELECT * FROM Customer", Encoding.ASCII);
-
-			SCommon.Batch(
-				new string[]
-				{
-					@"C:\Berry\app\sqlite-tools-win32-x86-3410000\sqlite3.exe DB < 1.txt > 2.txt"
-				},
-				@"C:\temp",
-				SCommon.StartProcessWindowStyle_e.MINIMIZED
-				);
-
-			rows2 = File.ReadAllLines(@"C:\temp\2.txt", Encoding.ASCII).Select(line =>
 			{
-				string[] row = line.Split('|');
+				File.WriteAllText(@"C:\temp\1.txt", "SELECT * FROM Customer", Encoding.ASCII);
 
-				for (int colidx = 1; colidx <= 6; colidx++)
-					row[colidx] = SCommon.ENCODING_SJIS.GetString(SCommon.Hex.ToBytes(row[colidx].Replace("-", "")));
+				SCommon.Batch(
+					new string[]
+					{
+						@"C:\Berry\app\sqlite-tools-win32-x86-3410000\sqlite3.exe DB < 1.txt > 2.txt"
+					},
+					@"C:\temp",
+					SCommon.StartProcessWindowStyle_e.MINIMIZED
+					);
 
-				return row;
-			})
-			.ToArray();
+				string[][] rows;
+
+				{
+					string[] lines = File.ReadAllLines(@"C:\temp\2.txt", Encoding.ASCII);
+
+					rows = new string[lines.Length][];
+
+					for (int index = 0; index < rows.Length; index++)
+					{
+						string line = lines[index];
+						string[] row = line.Split('|');
+
+						lines[index] = null;
+
+						for (int colidx = 1; colidx <= 6; colidx++)
+							row[colidx] = SCommon.ENCODING_SJIS.GetString(SCommon.Hex.ToBytes(row[colidx].Replace("-", "")));
+
+						rows[index] = row;
+					}
+				}
+
+				Array.Sort(rows, (a, b) => SCommon.Comp(a[0], b[0]));
+				using (CsvFileWriter writer = new CsvFileWriter(file2))
+				{
+					writer.WriteRows(rows);
+				}
+			}
 
 			// ----
 
-			Array.Sort(rows1, (a, b) => SCommon.Comp(a[0], b[0]));
-			Array.Sort(rows2, (a, b) => SCommon.Comp(a[0], b[0]));
+			string hash1 = SCommon.Hex.ToString(SCommon.GetSHA512File(file1));
+			string hash2 = SCommon.Hex.ToString(SCommon.GetSHA512File(file2));
 
-			if (SCommon.Comp(rows1, rows2, (a, b) => SCommon.Comp(a, b, SCommon.Comp)) != 0)
+			if (hash1 != hash2)
 				throw null; // BUG !!!
+
+			Console.WriteLine("OK!");
 		}
 	}
 }
