4a8e6edb1ab3d82ad6be63a73c6d92713e73ec6a
stackprobe <stackprobes@gmail.com>
2023-03-06 01:08:19 +0900

    Voyager 1 is 23824269079.136 kilometers away from the Sun.

---
+++ workspace/00_Console/src/tests/charlotte/commons/Test0006.java
@@ -0,0 +1,38 @@
+package tests.charlotte.commons;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test0006 {
+	public static void main(String[] args) {
+		try {
+			test01();
+		}
+		catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void test01() {
+		List<String> strs = new ArrayList<String>();
+
+		strs.add("AAA");
+		strs.add("BBB");
+		strs.add("CCC");
+
+		strs = strs.stream().toList();
+		//strs.add("DDD"); // 例外 -- unmodifiableList
+
+		for (String str : strs) {
+			System.out.println(str);
+		}
+
+		strs = strs.stream().collect(Collectors.toList());
+		strs.add("DDD"); // ok
+
+		for (String str : strs) {
+			System.out.println(str);
+		}
+	}
+}
